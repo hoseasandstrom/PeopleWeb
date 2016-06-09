80c23cba9663cd6734cc9a4695ea99b33da3b4c1
hoseasandstrom <Lakyn4ever>
2016-06-09 08:07:11 -0400

    Added buttons instead of hyperlinking the names

--- src/com/studenttheironyard/Main.java
+++ src/com/studenttheironyard/Main.java
@@ -1,93 +1,63 @@
 package com.studenttheironyard;
 
-import jodd.json.JsonSerializer;
-import jodd.util.MathUtil;
+
 import spark.ModelAndView;
 import spark.Session;
 import spark.Spark;
 import spark.template.mustache.MustacheTemplateEngine;
-
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
-    static HashMap<String, String> userMap = new HashMap<>();
-    static ArrayList<User> users = new ArrayList<>();
+    //static HashMap<String, String> userMap = new HashMap<>();
+    static ArrayList<User> userList = new ArrayList<>();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        readPeopleFile();
         Spark.init();
         Spark.get(
                 "/",
                 (request, response) -> {
-                    Session session = request.session();
-                    String username = session.attribute("username");
-
-                    String idStr = request.queryParams("userId");
-                    int userId = 0;
-                    if (idStr != null) {
-                        userId = Integer.valueOf(idStr);
+                    int offset = 0;
+                    String offStr = request.queryParams("offset");
+                    if (offStr != null) {
+                        offset = Integer.valueOf(offStr);
                     }
 
-                    ArrayList<User> subset = new ArrayList<User>();
-                    for (User usr : users) {
-                        if (usr.userId == userId) {
-                            subset.add(usr);
-                        }
-                    }
+                    ArrayList tempList = new ArrayList<>(userList.subList(offset, offset + 20));
 
-                    ArrayList arr = new ArrayList();
-                    arr.add(username);
+                    HashMap map = new HashMap();
+                    map.put("users", tempList);
+                    map.put("offsetUp", offset + 20);
+                    map.put("offsetDown", offset - 20);
+                    return new ModelAndView(map, "home.html");
+                },
+                new MustacheTemplateEngine()
+        );
 
-                    ArrayList<String> namesArr = new ArrayList<>(Arrays.asList(username));
-                    ArrayList<String> pair;
-                    while (namesArr.size() > 0) {
-                        pair = new ArrayList<>(namesArr.subList(0, 1));
-                        namesArr = new ArrayList<>(namesArr.subList(2, namesArr.size()));
-                        userMap.put(pair.get(0), pair.get(1));
-                    }
-                    return new ModelAndView(arr, "home.html");
+        Spark.get(
+                "/user",
+                (request, response) -> {
+                    int id = Integer.valueOf(request.queryParams("id"));
+                    User p = userList.get(id - 1);
+                    return new ModelAndView(p, "home.html");
                 },
                 new MustacheTemplateEngine()
         );
     }
-    public static HashMap<String, ArrayList<User>> readPeopleFromFile(String fileName) throws FileNotFoundException {
-        HashMap<String, ArrayList<User>> userMap = new HashMap<>();
-        File f = new File(fileName);
-        Scanner fileScanner = new Scanner(f);
+
+    public static void readPeopleFile() throws FileNotFoundException {
+        File people = new File("people.csv");
+        Scanner fileScanner = new Scanner(people);
+        fileScanner.nextLine();
         while (fileScanner.hasNext()) {
-            String line = fileScanner.nextLine();
-            String[] columns = line.split("\\,");
-            User user = new User(columns[0], columns[1], columns[2], columns[3], columns[4], Integer.valueOf(columns[5]),Integer.valueOf(columns[6]));
-            String peopleId = String.valueOf(user.firstName);
-            if (!userMap.containsKey(peopleId)) {
-                userMap.put(peopleId, new ArrayList<>());
-            }
-            userMap.get(peopleId).add(user);
+            String[] columns = fileScanner.nextLine().split(",");
+            User user = new User(Integer.valueOf(columns[0]), columns[1], columns[2], columns[3], columns[4], columns[5]);
+            userList.add(user);
+
         }
-        return userMap;
     }
-    public static void loadUser(String peopleId, String userCode ) throws IOException {
-        File peopleFile = new File(String.format("%s_people.csv", peopleId));
-
-        FileWriter fw = new FileWriter(peopleFile);
-        fw.write(peopleId);
-        fw.close();
-    }
-    public static void saveUser(ArrayList userArrayList, String peopleId) throws IOException {
-        File f = new File(String.format("%s_people.json", peopleId));
-        JsonSerializer serializer = new JsonSerializer();
-        String json = serializer.serialize(userArrayList);
-        FileWriter fw = new FileWriter(f);
-        fw.write(json);
-        fw.close();
-    }
-
-
-
 }
--- src/com/studenttheironyard/User.java
+++ src/com/studenttheironyard/User.java
@@ -7,21 +7,32 @@ import java.util.Scanner;
  * Created by hoseasandstrom on 6/8/16.
  */
 public class User {
-    String peopleId;
+    int id;
     String firstName;
     String lastName;
     String email;
     String country;
-    int uniqueUserId;
-    int userId;
+    String ipAddress;
 
-    public User(String peopleId, String firstName, String lastName, String email, String country, int uniqueUserId, int userId) {
-        this.peopleId = peopleId;
+
+    public User(int id, String firstName, String lastName, String email, String country, String ipAddress) {
+        this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
         this.country = country;
-        this.uniqueUserId = uniqueUserId;
-        this.userId = userId;
+        this.ipAddress = ipAddress;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", country='" + country + '\'' +
+                ", ipAddress='" + ipAddress + '\'' +
+                '}';
     }
 }
